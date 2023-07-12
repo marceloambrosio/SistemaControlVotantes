75c2d3a040928c7bafd52968885b29ff8ea66520
Marcelo Ambrosio <marceambrosio.ok@gmail.com>
2023-07-12 11:07:40 -0300

    first commit

---
+++ control/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+# Create your models here.
+
+class Seccion(models.Model):
+    num_seccion = models.IntegerField()
+    departamento = models.CharField()
+
+class Circuito(models.Model):
+    numero = models.IntegerField()
+    localidad = models.CharField(max_length=100)
+    seccion = models.ForeignKey(Seccion, on_delete=models.PROTECT)
+
+class Escuela(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre + " (" + self.direccion + ")"
+    
+class Mesa(models.Model):
+    num_mesa = models.IntegerField()
+    escuela = models.ForeignKey(Escuela, on_delete=models.PROTECT)
+    circuito = models.ForeignKey(Circuito, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return "Mesa Nº " + self.num_mesa
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    dni = models.IntegerField()
+    domiculio = models.CharField(max_length=100)
+    clase = models.IntegerField()
+
+    def __str__(self):
+        return self.apellido + ", " + self.nombre + " - DNI: " + str(self.dni)
+
+class PadronMesa(models.Model):
+    mesa = models.ForeignKey(Mesa, on_delete=models.PROTECT)
+    num_orden = models.IntegerField()
+    persona = models.ForeignKey(Persona, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.num_orden + " - " + self.persona.apellido + self.persona.nombre + " - " + self.persona.dni
