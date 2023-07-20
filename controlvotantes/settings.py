a2f247d4590f85938f1733318dafa6dfe70b102e
Marcelo Ambrosio <marceambrosio.ok@gmail.com>
2023-07-19 22:36:07 -0300

    allowed hosts * en settings

--- controlvotantes/settings.py
+++ controlvotantes/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 #ALLOWED_HOSTS = []
 #HABILITAR PARA PROBAR FUERA DE LA PC
-ALLOWED_HOSTS = ['localhost','127.0.0.1','192.168.9.42']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
